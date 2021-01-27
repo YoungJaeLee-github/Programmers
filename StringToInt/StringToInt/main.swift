4532228e30f3878a3fbc48bd004daea5d41b55e9
Youngjae Lee <77099686+YoungJaeLee-github@users.noreply.github.com>
2021-01-28 02:14:42 +0900

    문자열을 정수로 바꾸기

---
+++ StringToInt/StringToInt/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StringToInt
+//
+//  Created by 이영재 on 2021/01/28.
+//
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+print(solution("1234"))
+print(solution("-1234"))
